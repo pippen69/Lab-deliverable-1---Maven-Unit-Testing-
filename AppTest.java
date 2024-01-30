6284cb552e6815f2a832e42411cb93d0741392f6
pippen69 <84576402+pippen69@users.noreply.github.com>
2024-01-29 22:35:23 -0500

    Add files via upload

---
+++ AppTest.java
@@ -0,0 +1,20 @@
+package com.ontariotechu.sofe3980U;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
